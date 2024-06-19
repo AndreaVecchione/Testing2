20f7e4db2c32e926712ecc71d042d246ebbca7a2
AndreaVecchione <164344505+AndreaVecchione@users.noreply.github.com>
2024-06-20 00:38:32 +0200

    Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,14 @@ import java.time.format.FormatStyle;
 
 public class Main {
     public static void main(String[] args) {
+        //creo l'oggetto
         OffsetDateTime dateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
-
+//stampa della data in grandezza FULL
         String dateFull = dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
         System.out.println("Grandezza piena: " + dateFull);
 
     }
+    //metodo che andremo a testare nella classe MainTest
     static String formatInFull(OffsetDateTime dateTime) {
         return dateTime.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL));
     }
